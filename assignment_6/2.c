1086294ee2aeddf2a3d612ac773dfde981018f79
Dnyanada Joshi <joshi.dnyanada1996@gmail.com>
2017-10-26 02:23:14 +0530

    add q2

---
+++ assignment_6/2.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<sched.h>
+#include<sys/sysinfo.h>
+void *attach_thr(void *);
+int main() {
+    int nprocs = get_nprocs_conf(); /*Get number of processors*/
+    int i, arg[nprocs];
+    pthread_t thread_id[nprocs];
+    
+    printf("Number of CPUs : %d\n\n", nprocs);
+    
+    for(i = 0; i < nprocs; i++){
+        arg[i] = i;
+        pthread_create(&thread_id[i], NULL, attach_thr, &arg[i]);
+    }
+    
+    for(i = 0; i < nprocs; i++)
+        pthread_join(thread_id[i], NULL);
+    
+    return 0;
+}
+void *attach_thr(void *proc){
+    int cpu = *(int *)proc;
+    
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);  /*Clear cpuset */
+    CPU_SET(cpu, &cpuset); /*Set bit in 'cpuset', corresponding to 'cpu'*/
+
+    pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+    pthread_getaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
+
+    if(CPU_ISSET(cpu, &cpuset)){
+        printf("Thread attached successfully to CPU %d\n", cpu);
+    }
+    else
+        printf("Thread did not attach to CPU %d\n", cpu);
+}
