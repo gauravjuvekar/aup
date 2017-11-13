48f9c0d8253efb77d04956efad5eba32d9187a8c
Dnyanada Joshi <joshi.dnyanada1996@gmail.com>
2017-11-13 23:52:03 +0530

    Add q1

--- assignment_10/1.c
+++ assignment_10/1.c
@@ -2,7 +2,6 @@
 #include<stdio.h>
 #include<sys/wait.h>
 #include<string.h>
-
 int main(){
 
     int p1[2], p2[2], p3[2], p4[2];
@@ -11,6 +10,7 @@ int main(){
     pipe(p1);
     pipe(p2);
     pipe(p3);
+    pipe(p4);
     if((cpid1 = fork()) == 0){
         sprintf(buf, "%s", "Delhi\n");
         close(p2[0]);
@@ -18,11 +18,17 @@ int main(){
         close(p1[1]);
         read(p1[0], buf, 10);
         printf("String read from Parent by C1 : %s", buf);
+        sprintf(buf, "%s", "Chennai\n");
+        close(p4[0]);
+        write(p4[1], buf, strlen(buf));
     }
     else if((cpid2 = fork()) == 0){
         sprintf(buf, "%s", "Cochin\n");
         close(p3[0]);
         write(p3[1], buf, strlen(buf));
+        close(p4[1]);
+        read(p4[0], buf, 10);
+        printf("String read from C1 by C2 : %s", buf);
     
     }
     else {
